440ed72a857cc112768b4e44e9b8fb4b9162d8bc
maharajan1502 <129009154+maharajan1502@users.noreply.github.com>
2024-12-12 00:04:16 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './Components/Home'
 import About from './Components/About'
 import Services from './Components/Services'
 import Footer from './Components/Footer'
+import Rate from './Components/Rate'
 
 const App = () => {
   return (
     <div>
       <Home/>
       <About/>
+      <Rate/>
       <Services/>
       <Footer/>
     </div>
